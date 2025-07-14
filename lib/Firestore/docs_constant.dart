6db2f191ac701ce3bd3673869a5d224ea50042ca
massil30 <massilattaf04@gmail.com>
2025-07-14 09:48:19 +0100

    Adding User Model + Firestore Crud Operations

---
+++ lib/Firestore/docs_constant.dart
@@ -0,0 +1,10 @@
+// Collections
+const String userCollection = 'users';
+
+//User Collections
+const String userId = 'id';
+const String userFullName = 'full_name';
+const String userPhotoUrl = 'photo_url';
+const String userEmail = 'email';
+const String userFunction = 'function';
+// User Functions
---
+++ lib/Firestore/user_model.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_firebase/Firestore/docs_constant.dart';
+
+class UserModel {
+  final String? id;
+  final String full_name;
+  final String photo_url;
+  final String email;
+  final String function;
+
+  UserModel({
+    this.id,
+    required this.full_name,
+    required this.photo_url,
+    required this.email,
+    required this.function,
+  });
+
+  // Convert Firestore doc to UserModel
+  factory UserModel.fromMap(Map<String, dynamic> data, String docId) {
+    return UserModel(
+      id: docId,
+      full_name: data[userFullName] ?? '',
+      photo_url: data[userPhotoUrl] ?? '',
+      email: data[userEmail] ?? '',
+      function: data[userFunction] ?? '',
+    );
+  }
+
+  // Convert UserModel to Map
+  Map<String, dynamic> toMap() {
+    return {
+      userFullName: full_name,
+      userPhotoUrl: photo_url,
+      userEmail: email,
+      userFunction: function,
+      userId: id,
+    };
+  }
+}
